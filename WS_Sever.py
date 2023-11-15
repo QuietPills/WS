b7ac94c21a1aab00d638248a219a8af54261b1ac
Joe <mother@email.com>
2023-11-15 22:52:13 +0000

    a

--- WS_Sever.py
+++ WS_Sever.py
@@ -1,12 +1,20 @@
 import asyncio
+import sys
 from websockets.server import serve
+
+ip_addr = sys.argv[1]
+
+if ip_addr.replace(" ","") == "":
+    ip_addr = "localhost"
+
 
 async def echo(websocket):
     async for message in websocket:
+        print("Message Recived")
         await websocket.send(str(message) + "aa")
 
 async def main():
-    async with serve(echo, "localhost", 8765):
+    async with serve(echo, ip_addr, 8765):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
